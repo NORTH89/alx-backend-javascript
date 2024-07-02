973b22a7d8cebe84f60f0f03e0744ceb3e82be5c
NORTH89 <nordintata0@gmail.com>
2024-07-02 12:23:49 +0100

    fix output

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,13 +1,25 @@
-export default function updateStudentGradeByCity(students, city, newGrades) {
-  return students
+export default function updateStudentGradeByCity(
+  getListStudents,
+  city,
+  newGrades,
+) {
+  if (!Array.isArray(getListStudents) || !Array.isArray(newGrades)) return [];
+  return getListStudents
     .filter((student) => student.location === city)
     .map((student) => {
       const matchingGrade = newGrades.find(
         (grade) => grade.studentId === student.id,
       );
+      if (matchingGrade) {
+        return {
+          ...student,
+          grade:
+            matchingGrade.grade !== undefined ? matchingGrade.grade : 'N/A',
+        };
+      }
       return {
         ...student,
-        grade: matchingGrade ? matchingGrade.grade : 'N/A',
+        grade: 'N/A',
       };
     });
 }
