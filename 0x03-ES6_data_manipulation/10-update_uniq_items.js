cec7982f7fd9c4baafa9eba9cd512ef3f472bf5d
NORTH89 <nordintata0@gmail.com>
2024-07-02 12:26:29 +0100

    fix output

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,13 +1,11 @@
-function updateUniqueItems(map) {
-  if (!(map instanceof Map)) {
-    throw new Error('Cannot process');
-  }
-
-  for (const [key, value] of map) {
-    if (value === 1) {
-      map.set(key, 100);
+export default function updateUniqueItems(map) {
+  if (map instanceof Map) {
+    for (const [key, value] of map.entries()) {
+      if (value === 1) {
+        map.set(key, 100);
+      }
     }
+    return map;
   }
+  throw new Error('Cannot process ');
 }
-
-export default updateUniqueItems;
