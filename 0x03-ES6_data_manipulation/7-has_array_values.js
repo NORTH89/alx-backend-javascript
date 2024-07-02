9f2cdc371b1edd42fe050e36f0dea1b103f289ad
NORTH89 <nordintata0@gmail.com>
2024-07-02 12:22:33 +0100

    implement array function

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,3 +1,3 @@
 export default function hasValuesFromArray(set, array) {
-  return array.every((element) => set.has(element));
+  return array.reduce((acc, curr) => (acc && set.has(curr)), true);
 }
