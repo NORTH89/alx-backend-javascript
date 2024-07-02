6c614803d6379973a6f95ec0899fcd3e6663e01f
NORTH89 <nordintata0@gmail.com>
2024-07-02 10:36:00 +0100

    first tasks

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,18 @@
+const weakMap = new WeakMap();
+
+function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+
+  let queryCount = weakMap.get(endpoint);
+  queryCount += 1;
+
+  if (queryCount >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+
+  weakMap.set(endpoint, queryCount);
+}
+
+export { weakMap, queryAPI };
