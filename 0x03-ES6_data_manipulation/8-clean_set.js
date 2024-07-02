4fb05e1f83e7abbca877c013db9343175f40c9e3
NORTH89 <nordintata0@gmail.com>
2024-07-02 12:25:45 +0100

    fix output

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,17 +1,7 @@
-function cleanSet(set, startString) {
-  if (typeof startString !== 'string' || startString.length === 0) {
-    return '';
-  }
-
-  const result = [];
-
-  for (const value of set) {
-    if (value.startsWith(startString)) {
-      result.push(value.slice(startString.length));
-    }
-  }
-
-  return result.join('-');
+export default function cleanSet(set, startString) {
+  if (startString === undefined || startString.length === 0) return '';
+  return Array.from(set)
+    .filter((value) => (value ? value.startsWith(startString) : ''))
+    .map((value) => (value ? value.slice(startString.length) : ''))
+    .join('-');
 }
-
-export default cleanSet;
